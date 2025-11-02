e2dcd43198154b5fa5e59b2960dd65b55acf474d
XyrelTenz <xdemocrito1@gmail.com>
2025-11-02 18:06:35 +0800

    Day 4

--- lib/widget/sliver_appbar.dart
+++ lib/widget/sliver_appbar.dart
@@ -14,6 +14,31 @@ class _SilverAppbarState extends State<SilverAppbar> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Test")));
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: Icon(Icons.menu),
+              pinned: true,
+              floating: false,
+              elevation: 0,
+              expandedHeight: 200.0,
+              backgroundColor: Colors.purple[600],
+              // flexibleSpace: FlexibleSpaceBar(title: const Text("Title")),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => ListTile(
+                  leading: CircleAvatar(child: Text("${index + 1}")),
+                  title: Text("Item ${index + 1}"),
+                ),
+                childCount: 40,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
