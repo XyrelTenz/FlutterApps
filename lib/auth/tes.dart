150187120e0f2522ac93b6ab1e025d018d05b45b
XyrelTenz <xdemocrito1@gmail.com>
2025-11-05 20:03:50 +0800

    Day 8

---
+++ lib/auth/tes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Testing extends StatelessWidget {
+  const Testing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF0000FF),
+      child: Center(child: Text('Testing')),
+    );
+  }
+}
