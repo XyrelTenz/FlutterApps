754ebeba63fb1553c54f728f2d3c2c4cc923d3d6
XyrelTenz <xdemocrito1@gmail.com>
2025-11-11 13:14:12 +0800

    Day 13

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -18,7 +18,78 @@ class _UserLoginState extends State<UserLogin> {
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[Text("Login")],
+              spacing: 10,
+              children: <Widget>[
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF313131),
+                        ),
+                        borderRadius: BorderRadius.zero,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF313131),
+                        ),
+                      ),
+                      hintText: "Username",
+                      hintStyle: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF313131),
+                        ),
+                        borderRadius: BorderRadius.zero,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF313131),
+                        ),
+                      ),
+                      hintText: "Password",
+                      hintStyle: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: 50,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow[500],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Color(0xFF313131),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
