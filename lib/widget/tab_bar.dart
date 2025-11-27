37af02fd39ce66af8fa65419bb1ea8dcdd034452
XyrelTenz <xdemocrito1@gmail.com>
2025-11-27 13:39:32 +0800

    Facebook

--- lib/widget/tab_bar.dart
+++ lib/widget/tab_bar.dart
@@ -1,9 +1,25 @@
 import "package:flutter/material.dart";
 
-class TabBarWidget extends StatelessWidget {
+class TabBarWidget extends StatefulWidget {
   const TabBarWidget({super.key});
 
+  @override
+  State<TabBarWidget> createState() => _Facebook();
+}
+
+class _Facebook extends State<TabBarWidget> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   final _tabBar = 5;
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -12,10 +28,37 @@ class TabBarWidget extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Facebook"),
           bottom: TabBar(
-            tabs: <Widget>[Tab(icon: Icon(Icons.person_outline_outlined))],
+            indicatorColor: Color(0xFF0046FF),
+            labelColor: Color(0xFF0046FF),
+            unselectedLabelColor: Color(0xFF313131),
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.home_rounded)),
+              Tab(icon: Icon(Icons.people_rounded)),
+              Tab(icon: Icon(Icons.message_rounded)),
+              Tab(icon: Icon(Icons.notifications_rounded)),
+              Tab(icon: Icon(Icons.video_library_rounded)),
+            ],
           ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            _tabBody(name: "Home"),
+            _tabBody(name: "Friend"),
+            _tabBody(name: "Messages"),
+            _tabBody(name: "Notification"),
+            _tabBody(name: "Watch"),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget _tabBody({required String name}) {
+  return Center(
+    child: Text(
+      name,
+      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+  );
 }
